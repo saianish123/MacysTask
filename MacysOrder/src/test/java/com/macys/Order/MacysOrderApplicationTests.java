4a0cd74935b116b13e6de1fcd10f4ed3fb0d4346
saianish123 <97870832+saianish123@users.noreply.github.com>
2023-01-11 18:43:03 +0530

    Add files via upload

---
+++ MacysOrder/src/test/java/com/macys/Order/MacysOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macys.Order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacysOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
